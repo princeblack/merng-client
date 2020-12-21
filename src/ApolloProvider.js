9d04ea322d37b3ecf456e516e9744fe79fcde036
princeblack <mahamadicongo@hotmail.com>
2020-12-21 02:02:52 +0100

    proxy url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -4,7 +4,7 @@ import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from '@ap
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000',
+    uri: 'https://mighty-cliffs-44163.herokuapp.com/',
 })
 
 const authLink = setContext(()=>{
